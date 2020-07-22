6131e0faa3b7db645c43ba0c8b8c9e91055a027e
Duy <asianinvasion.le@gmail.com>
2020-07-22 15:29:24 -0400

    build shell of app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 import Job from './Job';
+import JobsPagination from './JobsPagination';
 
 function App() {
 	const [params, setParams] = useState({});
 	const [page, setPage] = useState(1);
 	const { jobs, loading, error } = useFetchJobs(params, page);
 	return (
-		<Container>
+		<Container className="my-4">
+			<h1 className="mb-4">GitHub Jobs</h1>
+			<JobsPagination page={page} setPage={setPage}></JobsPagination>
 			{loading && <h1>Loading...</h1>}
 			{error && <h1>Error. Please refresh the page</h1>}
 			{jobs.map(job => {
 				return <Job key={job.id} job={job} />
 			})}
+			<JobsPagination page={page} setPage={setPage}></JobsPagination>
 		</Container>
 	);
 }
